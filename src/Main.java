077a625b7acf7631096a1c4cbcbefcf6d89e8f67
madina1212 <halikovamadina656@gmail.com>
2022-11-29 19:21:19 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        String s1 = new String("java");
+        String s2 = new String("JAVA");
+        System.out.println("s1 == s2is:"+ s1 ==s2);
+    }
+}
